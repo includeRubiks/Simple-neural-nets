3dd74f85e3d2234bc376e6aee38c6a95fca1be5a
includeRubiks <159594845+includeRubiks@users.noreply.github.com>
2024-09-01 22:30:52 -0400

    Final variables and training weights
    Yay!

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,5 @@
 # Simple neural nets
 ## The simplest networks around
 ## Training
-Trained using a method very similar to gradient descent, where I add `learning rate` to each weight, then test, 
-see if the loss is less, and if it is, keep that weight that way, otherwise, subtract `learning rate`.
+Trained using a method very similar to gradient descent, where I test the loss of the result of adding  `learning rate` to each weight, and if loss is less, add learning rate.
 It's like gradient descent, but no calculus (which I don't understand).
--- neural nets/level1.py
+++ neural nets/level1.py
@@ -6,6 +6,7 @@ import matplotlib as mpl
 x = 1 # Input for model
 y = 1 # Output for model
 learning_rate = 0.01 # The learning rate for the model (Google has a good course outlining stuff like this)
+epochs = 100 # The number of times you train the model
 w = np.random.randint(0,10)*0.01 # The one weight needed for the network
 b = np.random.randint(0,10)*0.01 # The one bias needed for the network
 trainingx = [1,0] # Input for training model (trainingx[0]'s output should be aprox. to trainingy[0])
@@ -13,6 +14,17 @@ trainingy = [0,1] # Output the model is aiming for
 def sigmoid(a):
     # Defining sigmoid function. If you don't know what that is 3Blue1Brown has a youtube video on neural networks, which is very good to learn the math.
     return 1/(1+np.exp(-a))
-def output(a):
+def output(xi,wi,bi):
     # Defining the output function of the model
-    return sigmoid(w * a + b)
+    return sigmoid(wi * xi + bi)
+# Definitions done!
+# On to training
+for i in range(epochs):
+    #Train the weights
+    for i in range(len((trainingx+trainingy)/2)):
+        l2 = (output(trainingx[i],w,b)-trainingy[i])**2 # Find loss squared
+        if l2 > (output(trainingx[i],w+learning_rate,b)-trainingy[i])**2: # Compare the two losses
+            w += learning_rate # If it decreases loss, permanantly add to w so that it did
+        elif l2 > (output(trainingx[i],w-learning_rate,b)-trainingy[i])**2: # Compare the two losses
+            w -= learning_rate # If it decreases loss, permanantly subtract from w so that it did
+        
